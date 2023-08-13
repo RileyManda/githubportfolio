12d6f0c091ca5b53c98e9e4bd91f0053c33cfc58
RileyManda <rileymanda69@gmail.com>
2023-08-13 22:44:06 +0200

    Setup linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 function App() {
 
   return (
-    <>
-      <code>Riley + Manda</code>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   )
 }
 
