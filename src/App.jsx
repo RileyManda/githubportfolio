7638aa8e42c5dcc87a532282c8da10a56d128805
RileyManda <rileymanda69@gmail.com>
2023-08-13 22:58:35 +0200

    Updated router usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 import NotFound from './components/NotFound';
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
