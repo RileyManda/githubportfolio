e94db8b2119bb771c69d86bf57315bb301b0135a
RileyManda <rileymanda69@gmail.com>
2023-09-27 21:49:28 +0200

    Implemented banner animation

---
+++ src/components/Anim2.jsx
@@ -0,0 +1,11 @@
+import CodAnim from '../assets/cod.json';
+import Lottie from 'lottie-react';
+
+const Anim2 = () => {
+  return (
+    <div>
+      <Lottie animationData={CodAnim} />
+    </div>
+  )
+}
+export default Anim2;
---
+++ src/components/AnimData.jsx
@@ -0,0 +1,12 @@
+
+import Anim1 from './Anim1.jsx';
+import Anim2 from './Anim2.jsx';
+
+
+const AnimData = [
+  [Anim1],
+  [Anim2],
+
+];
+
+export default AnimData;
